87c25385365e971c153f1f4cf35e3824bdde39da
killianrazafimandrato-ux <killianrazafimandrato@gmail.com>
2026-07-07 13:34:01 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Joue à [.ggSkyLynx](https://discord.gg/mf2sXaJYwd)",
+        name: "Joue à PTFS SkyLynx RolePlay",
         // Activity type number (0 = Playing).
         type: 0,
       },
